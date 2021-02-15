6cb34338f02b2312ab767d7f3ae9a6f4743e5359
Changyao Chen <changyao.chen@gmail.com>
2021-02-15 14:36:20 -0500

    lint fix, move test to pytest

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-  name='rbo',
-  version='0.1.0',
-  packages=find_packages(),
-  install_requires=['numpy'],
-)
---
+++ tests/test.py
@@ -0,0 +1,50 @@
+"""
+This module contains some test cases.
+"""
+import string
+
+import numpy as np
+import pytest
+from rbo.rbo import RankingSimilarity
+
+TESTS = [
+    # Sanity checks
+    (string.ascii_lowercase, string.ascii_lowercase, 1.0),
+    (string.ascii_lowercase, string.ascii_lowercase[:7], 1.0),
+    ('abcde', 'fghij', 0.0),
+
+    # RBO Paper Figure 5
+    ('abcdefg', 'zcavwxy', 0.312),
+
+    # Source:  https://ragrawal.wordpress.com/2013/01/18/comparing-ranked-list/
+    ('abcde', 'bacde', 0.8),
+    ('abcde', 'abced', 0.95),
+
+    # One-Element lists
+    ('a', 'a', 1.0),
+    ('a', 'b', 0),
+
+    # Empty lists
+    ('', '', 1),
+    ('a', '', 0),
+    ('', 'a', 0),
+]
+
+
+@pytest.mark.parametrize('S, T, expected', TESTS)
+def test_rbo(S, T, expected):
+    p = 0.95
+    S, T = list(S), list(T)
+    print('List 1 is: {}'.format(S))
+    print('List 2 is: {}'.format(T))
+
+    RS = RankingSimilarity(S, T, verbose=True)
+    rbo = RS.rbo(p=1.0)
+    print('The implemented Average Overlap is: {:6.3f}'.format(rbo))
+    print('The correct answer is:              {:6.3f}'.format(expected))
+    assert np.round(rbo, decimals=3) == expected
+
+    print('The implemented rbo_ext 1 is: {:6.3f}'.format(
+        RS.rbo(p=p, k=3, ext=True)))
+    print('The implemented rbo_ext 2 is: {:6.3f}'.format(
+        RS.rbo_ext(p=p)))
